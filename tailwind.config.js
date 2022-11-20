ec756416678c589e5ede9b493eb59199b6c00eb6
George <gio.digmel@gmail.com>
2022-11-20 14:20:37 -0800

    chore: added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./pages/*.{js,ts,jsx,tsx}",
+    "./components/*.{js,ts,jsx,tsx}",
+    "./icons/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+  },
+  plugins: [],
+};
