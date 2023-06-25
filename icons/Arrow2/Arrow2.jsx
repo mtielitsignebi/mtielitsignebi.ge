69774bbb544b609c6baceedd2c2c89eb4bccd370
George <gio.digmel@gmail.com>
2023-06-25 01:47:09 -0500

    chore: added basic icons

---
+++ icons/Arrow2/Arrow2.jsx
@@ -0,0 +1,29 @@
+/*
+We're constantly improving the code you see. 
+Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcNg&d=1152665201300829
+*/
+
+import React from "react";
+
+export const Arrow2 = ({ style }) => {
+  return (
+    <svg
+      className="arrow-2"
+      style={style}
+      fill="none"
+      height="24"
+      viewBox="0 0 24 24"
+      width="24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        className="path"
+        d="M13.5 4.5L21 12M21 12L13.5 19.5M21 12H3"
+        stroke="white"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+      />
+    </svg>
+  );
+};
---
+++ icons/FacebookOriginal/FacebookOriginal.jsx
@@ -0,0 +1,45 @@
+/*
+We're constantly improving the code you see. 
+Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcNg&d=1152665201300829
+*/
+
+import React from "react";
+
+export const FacebookOriginal = ({ style }) => {
+  return (
+    <svg
+      className="facebook-original"
+      style={style}
+      fill="none"
+      height="24"
+      viewBox="0 0 24 24"
+      width="24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        className="path"
+        d="M22 12C22 6.47715 17.5229 2 12 2C6.47715 2 2 6.47715 2 12C2 16.9912 5.65684 21.1283 10.4375 21.8785V14.8906H7.89844V12H10.4375V9.79688C10.4375 7.29063 11.9305 5.90625 14.2146 5.90625C15.3084 5.90625 16.4531 6.10156 16.4531 6.10156V8.5625H15.1922C13.95 8.5625 13.5625 9.3334 13.5625 10.125V12H16.3359L15.8926 14.8906H13.5625V21.8785C18.3432 21.1283 22 16.9912 22 12Z"
+        fill="url(#paint0_linear_166_709)"
+      />
+      <path
+        className="path"
+        d="M15.8926 14.8906L16.3359 12H13.5625V10.125C13.5625 9.33418 13.95 8.5625 15.1922 8.5625H16.4531V6.10156C16.4531 6.10156 15.3088 5.90625 14.2146 5.90625C11.9305 5.90625 10.4375 7.29063 10.4375 9.79688V12H7.89844V14.8906H10.4375V21.8785C11.4729 22.0405 12.5271 22.0405 13.5625 21.8785V14.8906H15.8926Z"
+        fill="#F9FBFC"
+      />
+      <defs className="defs">
+        <linearGradient
+          className="linear_gradient"
+          gradientUnits="userSpaceOnUse"
+          id="paint0_linear_166_709"
+          x1="12"
+          x2="12"
+          y1="2"
+          y2="21.8785"
+        >
+          <stop className="stop" stopColor="#14A4FA" />
+          <stop className="stop" offset="1" stopColor="#016CE4" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
