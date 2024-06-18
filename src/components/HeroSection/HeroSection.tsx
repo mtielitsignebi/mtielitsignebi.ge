b8742c79be9b282f0bfda9fabe24b05a8691bc83
George <gio.digmel@gmail.com>
2024-06-17 19:47:19 -0700

    chore: configure video player

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,23 +1,51 @@
+import React, { useEffect } from "react";
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
+import "videojs-youtube";
 import {
   HeroSectionWrapper,
-  HeroImage,
-  PlayIconWrapper,
   HeroContent,
   Heading,
+  VideoWrapper,
 } from "./styles";
-import { Playicon } from "../../icons/Playicon";
 import { usePlatform } from "../../hooks";
 
-export const HeroSection = (): JSX.Element => {
+export const HeroSection = () => {
   const platform = usePlatform();
+
+  useEffect(() => {
+    const videoElement = document.getElementById("my-video");
+    if (videoElement) {
+      videojs(videoElement, {
+        techOrder: ["youtube"],
+        fluid: true,
+        controls: true,
+        sources: [
+          {
+            src: "https://www.youtube.com/watch?v=nc-2p1AaTX4",
+            type: "video/youtube",
+          },
+        ],
+        controlBar: {
+          volumePanel: {
+            inline: false,
+          },
+        },
+      });
+    }
+  }, []);
 
   return (
     <HeroSectionWrapper platform={platform} className="hero-section">
-      <HeroImage platform={platform} className="hero-image">
-        <PlayIconWrapper className="play-icon">
-          <Playicon className="" />
-        </PlayIconWrapper>
-      </HeroImage>
+      <VideoWrapper platform={platform}>
+        <video
+          id="my-video"
+          className="video-js vjs-big-play-centered"
+          controls
+          preload="auto"
+        />
+      </VideoWrapper>
+
       <HeroContent platform={platform} className="hero">
         <Heading platform={platform} className="heading">
           ჩვენი მიზანია, შევიძინოთ, შევაგროვოთ საინტერესო და სასურველი
