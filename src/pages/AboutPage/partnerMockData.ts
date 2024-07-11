55de4320e0479aa4f000ee7b1a1a11dd7849b19c
George <gio.digmel@gmail.com>
2024-07-10 22:37:10 -0700

    fix: about page responsivnes

---
+++ src/pages/AboutPage/partnerMockData.ts
@@ -0,0 +1,58 @@
+export const partnerMockData = [
+  {
+    name: "მწვანე სკოლა",
+    logo: "https://c.animaapp.com/rsy2ONkj/img/logo-1@2x.png",
+  },
+  {
+    name: "21-ე საჯარო სკოლა",
+    logo: "https://c.animaapp.com/rsy2ONkj/img/logo-2@2x.png",
+  },
+  {
+    name: "ორბელიანი მეტი",
+    logo: "https://c.animaapp.com/rsy2ONkj/img/logo-3@2x.png",
+  },
+  {
+    name: "ლიბერთი ბანკი",
+    logo: "https://c.animaapp.com/rsy2ONkj/img/logo-4@2x.png",
+  },
+  {
+    name: "მჭევრის დამოუკიდებელი ბიზნეს სკოლა",
+    logo: "https://c.animaapp.com/rsy2ONkj/img/logo-5@2x.png",
+  },
+  {
+    name: "The Guivy Zaldastanishvili American Academy",
+    logo: "https://c.animaapp.com/rsy2ONkj/img/logo-6@2x.png",
+  },
+  {
+    name: "Combined Ratio Solutions",
+    logo: "https://c.animaapp.com/rsy2ONkj/img/logo-7@2x.png",
+  },
+  {
+    name: "Swiss Capital",
+    logo: "https://c.animaapp.com/rsy2ONkj/img/logo-8@2x.png",
+  },
+  {
+    name: "თეგეტა მოტორსი",
+    logo: "https://c.animaapp.com/rsy2ONkj/img/logo-9@2x.png",
+  },
+  {
+    name: "USAID Civil Society Engagement Program",
+    logo: "https://c.animaapp.com/rsy2ONkj/img/logo-10@2x.png",
+  },
+  {
+    name: "გაეროს გლობალური შეთანხმების საქართველოს ქსელი",
+    logo: "https://c.animaapp.com/rsy2ONkj/img/logo-11@2x.png",
+  },
+  {
+    name: "აკაკი კივილაძის სახელობის თბილისის N8 საჯარო სკოლა",
+    logo: "https://c.animaapp.com/rsy2ONkj/img/logo-12@2x.png",
+  },
+  {
+    name: "წმ.გიორგის სახელობის საერთაშორისო სკოლა",
+    logo: "https://c.animaapp.com/rsy2ONkj/img/logo-13@2x.png",
+  },
+  {
+    name: "GELi . საქართველოს ეკონომიკური წიგნიერების ინიციატივა",
+    logo: "https://c.animaapp.com/rsy2ONkj/img/logo-14@2x.png",
+  },
+];
